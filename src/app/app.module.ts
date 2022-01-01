25f08643b16e290121d13ac648dc38a68ff5528c
RamiKoh <kohil.ikram@hotmail.com>
2022-01-01 18:32:41 -0500

    Added about me a other minor adjustments like custom scrollbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { TitlePageComponent } from './pages/title-page/title-page.component';
+import { AboutMePageComponent } from './pages/about-me-page/about-me-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     SideNavigationComponent,
+    TitlePageComponent,
+    AboutMePageComponent,
   ],
   imports: [
     BrowserModule,
