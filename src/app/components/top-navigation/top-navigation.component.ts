7c9a1cd941a2f54c1a0cf9d9970363a6cfdbe010
RamiKoh <kohil.ikram@hotmail.com>
2022-01-04 18:16:35 -0500

    ADjusted nav bar and cursor

--- src/app/components/top-navigation/top-navigation.component.ts
+++ src/app/components/top-navigation/top-navigation.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewPortScroller: ViewportScroller) { }
 
   ngOnInit(): void {
   }
 
+  scroll() : void {
+    this.viewPortScroller.scrollToAnchor('title-page');
+  }
 }
