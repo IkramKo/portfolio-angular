5e04c2535b98a9d5f0f5d325f99b8645dd8e2674
RamiKoh <kohil.ikram@hotmail.com>
2022-01-05 15:09:48 -0500

    Loaded projects on init in projects card

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import JSONProjects from '@assets/json/projects.json';
+
+interface ProjectInfo {
+  title: string,
+  image: string,
+  description: string,
+  sourceCode: string,
+  link: string
+}
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
+
 export class ProjectCardComponent implements OnInit {
-
+  projectsList : ProjectInfo[] = [];
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.createProjects();
   }
+  
+  private createProjects() : void {
+    const projectsNbr: number = Object.keys(JSONProjects).length;
 
+    for (let i = 0; i < projectsNbr; i++) {
+      this.projectsList[i] = JSON.parse(JSON.stringify(JSONProjects[i]));
+    }
+  }
 }
