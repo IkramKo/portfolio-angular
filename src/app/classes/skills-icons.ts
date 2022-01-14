737ccfe40de00754f5e37e6bf4716f6b9ad17ead
RamiKoh <kohil.ikram@hotmail.com>
2022-01-13 20:53:20 -0500

    Added skills list to about me page

--- src/app/classes/skills-icons.ts
+++ src/app/classes/skills-icons.ts
@@ -2,15 +2,15 @@ export enum SkillsIcons {
     Ts = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
     Angular = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg',
     NodeJs = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
-    Git = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
-    Jira = 'ttps://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg',
     Html = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
     Css = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
     Cpp = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg',
     C = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg',
     Python = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
-    Flutter = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg',
     Java = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
+    Flutter = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg',
     Sql = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
-    Linux = 'img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg'
+    Git = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+    Jira = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg',
+    Linux = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg'
 }
--- src/app/classes/skills.ts
+++ src/app/classes/skills.ts
@@ -7,5 +7,16 @@ export interface SkillsInfo {
 export const SKILLS_LIST: SkillsInfo[] = [
     { icon: SkillsIcons.Ts, name: 'TypeScript'},
     { icon: SkillsIcons.Angular, name: 'Angular'},
-    { icon: SkillsIcons.NodeJs, name: 'NodeJs'}
+    { icon: SkillsIcons.NodeJs, name: 'NodeJs'},
+    { icon: SkillsIcons.Html, name: 'HTML5'},
+    { icon: SkillsIcons.Css, name: 'CSS'},
+    { icon: SkillsIcons.Cpp, name: 'C++'},
+    { icon: SkillsIcons.C, name: 'C'},
+    { icon: SkillsIcons.Python, name: 'Python'},
+    { icon: SkillsIcons.Java, name: 'Java'},
+    { icon: SkillsIcons.Flutter, name: 'Flutter'},
+    { icon: SkillsIcons.Sql, name: 'MYSQL'},
+    { icon: SkillsIcons.Git, name: 'Git'},
+    { icon: SkillsIcons.Jira, name: 'Jira'},
+    { icon: SkillsIcons.Linux, name: 'Linux'}
 ];
